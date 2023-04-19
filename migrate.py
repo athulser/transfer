f9cb99754808c3b8bb78f227ecde4d706d825b2e
athulser <97336138+athulser@users.noreply.github.com>
2023-04-20 00:54:06 +0530

    Add files via upload

---
+++ migrate.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+cluster = MongoClient("mongodb+srv://tzvri75136:Atulrv2005@mortydb.t0mwlvs.mongodb.net/?retryWrites=true&w=majority")
+db = cluster['mortydb']
+collection_groups = db['groups']
+collection_users = db['users']
+
+# 0 is FALSE
+# 1 is TRUE
+
+for document in collection_groups.find({}):
+    to_write = {
+        'id' : f'{document["id"]}',
+        'general':{'clean_mode':'off',
+                   'get_updates':'on',
+                   'chat_action':'on',
+                   'progress_animation':'off',
+                   'welcome':'on',
+                   'welcome_message':'Hey [NAME], Welcome to [GROUPNAME], Enjoy your stay'
+        },
+        'play': {'max_results':5},
+        'image' : {'nsfw_filter':'on'} 
+    }
+    collection_groups.update_one({'id':f'{document["id"]}'}, {'$set':to_write})
+print("Updated")
+    
+
+
+
+    
+
+
+
+
